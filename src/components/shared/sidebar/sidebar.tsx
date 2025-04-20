dfd2249b3bbfef9bd9c4ff4e10715d27b559b206
Kingsley Ifijeh <kinxly@gmail.com>
2025-04-20 23:09:51 +0100

    init: scarfold hris folder structure

--- src/components/shared/sidebar/sidebar.tsx
+++ src/components/shared/sidebar/sidebar.tsx
@@ -16,7 +16,6 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 // import { Logo } from "../logo";
-import { SetToolTip } from "../tool-tip";
 
 export function DashboardSidebar({ navItems }: { navItems: any }) {
   const pathname = usePathname();
@@ -65,33 +64,31 @@ export function DashboardSidebar({ navItems }: { navItems: any }) {
 
             return (
               <SidebarMenuItem key={item.id}>
-                <SetToolTip content={item.route}>
-                  <SidebarMenuButton
-                    asChild
-                    className={cn(
-                      "flex h-[48px] items-center gap-3 rounded-lg text-[16px] font-medium transition-all duration-200",
-                      isActive
-                        ? "border-primary text-primary shadow-active border-2"
-                        : "text-mid-grey-II hover:bg-low-grey-I",
+                <SidebarMenuButton
+                  asChild
+                  className={cn(
+                    "flex h-[48px] items-center gap-3 rounded-lg text-[16px] font-medium transition-all duration-200",
+                    isActive
+                      ? "border-primary text-primary shadow-active border-2"
+                      : "text-mid-grey-II hover:bg-low-grey-I",
+                  )}
+                >
+                  <Link onClick={handleCloseOnMobile} href={link} data-testid={item.id} role="sidebar-link">
+                    {/* {renderIcon(item)} */}
+                    <span className={`font-medium dark:text-white`}>{item.route}</span>
+                    {item.badge && (
+                      <SidebarMenuBadge
+                        className={cn(
+                          "absolute right-2 flex h-5 w-5 items-center justify-center rounded-full text-xs",
+                          item.badge.variant === "danger" ? "bg-mid-danger text-white" : "bg-gray-200",
+                          item.badge.count === 0 && "hidden",
+                        )}
+                      >
+                        {item.badge.count}
+                      </SidebarMenuBadge>
                     )}
-                  >
-                    <Link onClick={handleCloseOnMobile} href={link} data-testid={item.id} role="sidebar-link">
-                      {/* {renderIcon(item)} */}
-                      <span className={`font-medium dark:text-white`}>{item.route}</span>
-                      {item.badge && (
-                        <SidebarMenuBadge
-                          className={cn(
-                            "absolute right-2 flex h-5 w-5 items-center justify-center rounded-full text-xs",
-                            item.badge.variant === "danger" ? "bg-mid-danger text-white" : "bg-gray-200",
-                            item.badge.count === 0 && "hidden",
-                          )}
-                        >
-                          {item.badge.count}
-                        </SidebarMenuBadge>
-                      )}
-                    </Link>
-                  </SidebarMenuButton>
-                </SetToolTip>
+                  </Link>
+                </SidebarMenuButton>
               </SidebarMenuItem>
             );
           })}
